c0947497020de83e3ea3febdf63cc2fdfbda8ca6
BigBinary <abhishekt@Abhisheks-MBP.bbrouter>
2021-09-08 12:07:14 +0530

    Set up react environment

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
+// previous code if any
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  // previous code if any
+
+  useEffect(() => {
+    initializeLogger();
+    setAuthHeaders(setLoading);
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // previous code without changes
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
